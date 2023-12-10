714e7343c2acfc9f3b729cf67d4359b3015e3655
Buzeinep Ysmanalieva <90492820+Disimparato@users.noreply.github.com>
2023-12-10 17:55:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var video = document.getElementById("myVideo");
+
+    // Play the video when the page loads
+    video.play();
+});
